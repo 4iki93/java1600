b428a9584b81c3437ee5545f69c125d7774b7c3f
4iki93 <4iki93@gmail.com>
2017-10-09 08:46:37 +0300

    first commit

---
+++ src/main/java/ua/com/owu/servlet/Servlet.java
@@ -0,0 +1,19 @@
+package ua.com.owu.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "Servlet")
+public class Servlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            request.getRequestDispatcher("/WEB-INF/pages/userPage.jsp").forward(request,response);
+    }
+}
---
+++ target/java1600-1.0-SNAPSHOT/WEB-INF/pages/index.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+ hello
+<br>
+<a href="/next">user pages</a>
+</body>
+</html>
